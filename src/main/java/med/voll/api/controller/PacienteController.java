d7d574c2b57dac8b50127302be0baf93f2d6fcda
Adson Conceição <adsonconcecao@yahoo.com.br>
2023-05-06 19:59:32 -0300

    Criando ResponseEntity de HTTP

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,8 +25,9 @@ private PacienteRepository repository;
 
 
     @GetMapping
-    public Page<DaddosListarPaciente> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao ) {
-        return repository.findAllByAtivoTrue(paginacao).map(DaddosListarPaciente::new);
+    public ResponseEntity<Page<DaddosListarPaciente>> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao ) {
+        var page = repository.findAllByAtivoTrue(paginacao).map(DaddosListarPaciente::new);
+        return ResponseEntity.ok(page);
     }
 
     @PutMapping
@@ -37,9 +39,11 @@ private PacienteRepository repository;
 
     @DeleteMapping("/{id}")
     @Transactional
-    public void remover(@PathVariable Long id) {
+    public ResponseEntity remover(@PathVariable Long id) {
         var paciente = repository.getReferenceById(id);
         paciente.inativar();
+
+        return ResponseEntity.noContent().build();
     }
 
 }
