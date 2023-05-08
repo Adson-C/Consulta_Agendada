4ccb0bb991fe6ee812265fa834962b6cab021e3b
Adson Conceição <adsonconcecao@yahoo.com.br>
2023-05-08 18:09:48 -0300

    Criando Class/Controlles/Repository/DTO Consultas

---
+++ src/main/java/med/voll/api/domain/consulta/DadosdetalhamentoConsultas.java
@@ -0,0 +1,24 @@
+package med.voll.api.domain.consulta;
+
+import java.time.LocalDateTime;
+
+public record DadosdetalhamentoConsultas(Long id, Long idMedico, Long idPaciente, LocalDateTime data) {
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
